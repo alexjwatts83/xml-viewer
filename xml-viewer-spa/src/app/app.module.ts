c25ab01d2beb2a9246abed6cb23bba1b46bccdc5
alexjwatts83 <alexjwatts83@gmail.com>
2020-11-16 20:40:12 +1100

    wip prismjs

--- xml-viewer-spa/src/app/app.module.ts
+++ xml-viewer-spa/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightService } from './highlight.service';
+import { BlogpostComponent } from './blogpost/blogpost.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogpostComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [HighlightService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
